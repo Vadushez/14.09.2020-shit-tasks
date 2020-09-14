9e3be3a98fd4e9d5a0983eeae5f6e251ff136f58
Vadushez <kopalka1998@gmail.com>
2020-09-14 06:51:15 +0000

    ez

--- main.cpp
+++ main.cpp
@@ -1,4 +1,19 @@
+#define базарю cin>>
+#define авдруг if
+#define базарь cout<<
+#define чиселко int
+#define погоняло string
+#define базар bool
+#define мамойклянусь true
+#define шакалится false
+#define шмон ==
+#define чееще &&
+#define почемутак else
+#define мутим for
+
+
 #include <iostream>
+
 using namespace std;
 int main() {
   
